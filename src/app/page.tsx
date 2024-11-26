21f112febef619524e41661c572eefda75806f55
Andy Frith <andy_frith@yahoo.com>
2024-11-26 07:51:11 -0700

    Changed initial text diaplaed when loaded.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="">
-      <main className="">Hello!</main>
+      <main className="">Next.js Starter</main>
       <footer className=""></footer>
     </div>
   );
